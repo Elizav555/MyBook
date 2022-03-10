3605c8313d7d1f963f0d5685553592eaf03c3e8c
Azaliya Khismatullina <newtmas1956@mal.ru>
2022-03-10 22:19:58 +0300

    [add] BookPage

---
+++ MyBook/Controllers/BookController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyBook.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET
+        public IActionResult Book()
+        {
+            return View();
+        }
+    }
+}
