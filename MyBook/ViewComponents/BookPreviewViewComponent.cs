682c80b504d326ae69c44cc2bbf05dce581a250a
DamirGuzaerov <86714479+DamirGuzaerov@users.noreply.github.com>
2022-03-13 16:43:03 +0300

    book preview card added

---
+++ MyBook/ViewComponents/BookPreviewViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using Microsoft.AspNetCore.Mvc;
+using MyBook.Entities;
+
+namespace MyBook.ViewComponents;
+
+public class BookPreviewViewComponent:ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync(Book book)
+    {
+        return View(book);
+    }
+}
