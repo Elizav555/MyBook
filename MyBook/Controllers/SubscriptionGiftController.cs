b754132d774be57b2ac96a79e7d492de2bf05a4d
Azaliya Khismatullina <newtmas1956@mal.ru>
2022-03-15 22:12:52 +0300

    [add] subscription pages

---
+++ MyBook/Controllers/SubscriptionGiftController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyBook.Controllers
+{
+    public class SubscriptionGiftController : Controller
+    {
+        // GET
+        public IActionResult SubscriptionGift()
+        {
+            return View();
+        }
+    }
+}
