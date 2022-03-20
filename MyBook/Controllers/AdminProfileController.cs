2ccb0024da44c489b282d29fb6eaa5502a6cb206
LeraRakhmankulova <lerakhmankulova@mail.ru>
2022-03-20 12:20:43 +0300

    admin pages & validation forms added

---
+++ MyBook/Controllers/AdminProfileController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyBook.Controllers
+{
+    public class AdminProfileController : Controller
+    {
+        public IActionResult EditAuthor()
+        {
+            return View();
+        }
+
+        public IActionResult EditBook()
+        {
+            return View();
+        }
+
+        public IActionResult EditUser()
+        {
+            return View();
+        }
+
+        public IActionResult EditBookPlace()
+        {
+            return View();
+        }
+
+        public IActionResult EditSubscription()
+        {
+            return View();
+        }
+    }
+}
