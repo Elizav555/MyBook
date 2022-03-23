e37c3311df92c655466048155802fb454a523793
DamirGuzaerov <86714479+DamirGuzaerov@users.noreply.github.com>
2022-03-23 19:02:47 +0300

    top books page skeleton added

---
+++ MyBook/ViewComponents/BooksCarouselViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBook.Entities;
+
+namespace MyBook.ViewComponents;
+
+public class BooksCarouselViewComponent: ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync(List<Book> book)
+    {
+        return View(book);
+    }
+}
