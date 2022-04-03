ed591accc756e64fbd5b65d8ec46a7fe549e25f4
Azaliya Khismatullina <newtmas1956@mal.ru>
2022-04-03 19:08:09 +0300

    [add] language filter to Library page

--- MyBook/Controllers/LibraryController.cs
+++ MyBook/Controllers/LibraryController.cs
@@ -1,32 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using MyBook.Entities;
 using MyBook.ViewModels;
 using Repositories;
 
-namespace MyBook.Controllers;
-
-public class LibraryController: Controller
+namespace MyBook.Controllers
 {
-    private readonly IGenericRepository<Book> _bookRepository;
-    private readonly IGenericRepository<Author> _authorRepository;
-    private readonly IGenericRepository<Genre> _genreRepository;
-    private readonly LibraryVIewModel _vIewModel;
-    
-    public LibraryController(
-        IGenericRepository<Book> bookRepository,
-        IGenericRepository<Author> authorRepository,
-        IGenericRepository<Genre> genreRepository)
-    {
-        _bookRepository = bookRepository;
-        _authorRepository = authorRepository;
-        _genreRepository = genreRepository;
-        _vIewModel = new LibraryVIewModel(bookRepository,authorRepository,genreRepository);
-    }
 
-    public async Task<IActionResult> Index()
+    public class LibraryController : Controller
     {
-        return View(_vIewModel);
-    } 
-    
-    
+        private readonly IGenericRepository<Book> _bookRepository;
+        private readonly IGenericRepository<Author> _authorRepository;
+        private readonly IGenericRepository<Genre> _genreRepository;
+        private LibraryVIewModel _vIewModel;
+
+        public LibraryController(IGenericRepository<Book> bookRepository,
+            IGenericRepository<Author> authorRepository,
+            IGenericRepository<Genre> genreRepository)
+        {
+            _bookRepository = bookRepository;
+            _authorRepository = authorRepository;
+            _genreRepository = genreRepository;
+        }
+
+        public async Task<IActionResult> Index(string searchString)
+        {
+
+            if (String.IsNullOrEmpty(searchString) || searchString=="Все")
+            {
+                _vIewModel = new LibraryVIewModel(_bookRepository, _authorRepository, _genreRepository);
+            }
+            else
+            {
+                _vIewModel = new LibraryVIewModel(_bookRepository, _authorRepository,
+                    _genreRepository, searchString);
+            }
+
+            return View(_vIewModel);
+        }
+    }
 }
--- MyBook/ViewModels/LibraryVIewModel.cs
+++ MyBook/ViewModels/LibraryVIewModel.cs
@@ -1,47 +1,110 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using MyBook.Entities;
 using Repositories;
 
-namespace MyBook.ViewModels;
 
-public class LibraryVIewModel
+namespace MyBook.ViewModels
 {
-    public List<Book> AllBooks { get; set; }
-    public List<Author> AllAuthors { get; set; }
-    public List<Genre> AllGenres { get; set; }
+    public class LibraryVIewModel
+    {
+        public List<Book> AllBooks { get; set; } 
+        public List<Author> AllAuthors { get; set; }
+        public List<Genre> AllGenres { get; set; }
 
-    public LibraryVIewModel(
-        IGenericRepository<Book> bookRepository,
-        IGenericRepository<Author> authorRepository,
-        IGenericRepository<Genre> genreRepository)
-    {
-        AllBooks = GetAllBooks(bookRepository);
-        AllAuthors = GetAllAuthors(authorRepository);
-        AllGenres = genreRepository.Get().ToList();
-    }
+        private readonly IGenericRepository<Book> _bookRepository;
+        private readonly string _searchString;
+        public List<Book> Books { get; set; } = new();
 
-    private List<Book> GetAllBooks(IGenericRepository<Book> bookRepository)
-    {
-        return bookRepository.GetWithMultiIncluding(
-            book => book,
-            book => true,
-            books => 
-                books.Include(book => book.AuthorBooks)
-                    .ThenInclude(authorBook =>  authorBook.Author)
-                    .Include(book => book.ImgLinks)
-                    
-        ).ToList();
-    }
-    
-    private List<Author> GetAllAuthors(IGenericRepository<Author> authorRepository)
-    {
-        return authorRepository.GetWithMultiIncluding(
-            author => author,
-            author => true,
-            authors => 
-                authors.Include(author => author.AuthorBooks)
-                    .ThenInclude(authorBook =>  authorBook.Book)
-                    .Include(author => author.ImgLinks)
-        ).ToList();
+        public  List<SelectListItem> dropdownItems = new List<SelectListItem>();
+
+
+        private List<Book> GetFilterBooks()
+        {
+            return _bookRepository.GetWithMultiIncluding(
+                book => book,
+                book => book.Language.EndsWith(_searchString),
+                books =>
+                    books.Include(book => book.AuthorBooks)
+                        .ThenInclude(authorBook => authorBook.Author)
+                        .Include(book => book.ImgLinks)
+                        .Include(book => book.Description)
+            ).ToList();
+        }
+
+        public LibraryVIewModel(
+            IGenericRepository<Book> bookRepository,
+            IGenericRepository<Author> authorRepository,
+            IGenericRepository<Genre> genreRepository,
+            string searchString)
+        {
+            _bookRepository = bookRepository;
+            _searchString = searchString;
+            dropdownItems = GetLanguages();
+            AllBooks = GetFilterBooks();
+            AllAuthors = GetAllAuthors(authorRepository);
+            AllGenres = genreRepository.Get().ToList();
+            Books = GetFilterBooks();
+        }
+        
+        public LibraryVIewModel(
+            IGenericRepository<Book> bookRepository,
+            IGenericRepository<Author> authorRepository,
+            IGenericRepository<Genre> genreRepository)
+        {
+            _bookRepository = bookRepository;
+            AllBooks = GetAllBooks(bookRepository);
+            AllAuthors = GetAllAuthors(authorRepository);
+            AllGenres = genreRepository.Get().ToList();
+            Books = GetFilterBooks();
+            dropdownItems = GetLanguages();
+        }
+
+
+        private List<SelectListItem> GetLanguages()
+        {
+            dropdownItems.Add(new SelectListItem() {Text = "Все", Value = "Все"});
+            List<Book> allBooks = GetAllBooks(_bookRepository);
+            List<string> languages = new List<string>();
+            foreach (var book in allBooks)
+            {
+                if (!languages.Contains(book.Language))
+                {
+                    languages.Add(book.Language);
+                }
+            }
+            foreach (var lang in languages)
+            {
+                var item = new SelectListItem() {Text = $"{lang}", Value = $"{lang}"};
+                dropdownItems.Add(item);
+            }
+            return dropdownItems;
+        }
+        private List<Book> GetAllBooks(IGenericRepository<Book> bookRepository)
+        {
+            return bookRepository.GetWithMultiIncluding(
+                book => book,
+                book => true,
+                books =>
+                    books.Include(book => book.AuthorBooks)
+                        .ThenInclude(authorBook => authorBook.Author)
+                        .Include(book => book.ImgLinks)
+
+            ).ToList();
+        }
+
+        private List<Author> GetAllAuthors(IGenericRepository<Author> authorRepository)
+        {
+            return authorRepository.GetWithMultiIncluding(
+                author => author,
+                author => true,
+                authors =>
+                    authors.Include(author => author.AuthorBooks)
+                        .ThenInclude(authorBook => authorBook.Book)
+                        .Include(author => author.ImgLinks)
+            ).ToList();
+        }
     }
 }
