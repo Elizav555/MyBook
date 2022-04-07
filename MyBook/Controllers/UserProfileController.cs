c67117443f93cdf2068dbce0e7b497cce074139e
Elizav555 <78415957+Elizav555@users.noreply.github.com>
2022-04-07 22:13:19 +0300

    in progress

--- MyBook/Controllers/UserProfileController.cs
+++ MyBook/Controllers/UserProfileController.cs
@@ -1,15 +1,96 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MyBook.Entities;
+using MyBook.Models;
+using System.Text.RegularExpressions;
 
 namespace MyBook.Controllers
 {
     [Authorize(Policy = "ReadersOnly")]
     public class UserProfileController : Controller
     {
-        public IActionResult EditProfile()
+        private readonly UserManager<User> _userManager;
+        public UserProfileController(UserManager<User> userManager)
         {
-            return View();
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditProfile(string id)
+        {
+            User user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            EditProfileViewModel model = new EditProfileViewModel
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                BirthDate = DateTime.Parse(user.BirthDate)
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditProfile(EditProfileViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await _userManager.FindByIdAsync(model.Id);
+                if (user != null)
+                {
+                    user.Email = model.Email;
+                    user.UserName = model.Email;
+                    user.BirthDate = model.BirthDate.ToShortDateString();
+                    user.FirstName = model.FirstName;
+                    user.LastName = model.LastName;
+
+                    var result = await _userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("EditProfile");
+                    }
+                    else
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty, error.Description);
+                        }
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditPassword(EditPasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await _userManager.FindByIdAsync(model.Id);
+                if (user != null)
+                {
+                    var result = await _userManager.ChangePasswordAsync(user, model.OldPassword, model.NewPassword);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("EditProfile",model.Id);
+                    }
+                    else
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            // ModelState.AddModelError(string.Empty, "Введите верный старый пароль");
+                            ModelState.AddModelError(string.Empty, error.Description);
+                        }
+                    }
+                }
+            }
+            return RedirectToAction("EditProfile");
         }
 
         public IActionResult EditSubscription()
