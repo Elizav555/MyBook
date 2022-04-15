ea3be3abb8919ab679a5802300174736d140d4e8
Elizav555 <lizagarkina5@gmail.com>
2022-04-15 21:17:10 +0300

    conflicts resolved

--- MyBook/Controllers/AccountController.cs
+++ MyBook/Controllers/AccountController.cs
@@ -9,101 +9,101 @@ namespace MyBook.Controllers
 {
     public class AccountController : Controller
     {
-        //private readonly UserManager<User> _userManager;
-        //private readonly SignInManager<User> _signInManager;
-        //private readonly MyBookContext _bookContext;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly MyBookContext _bookContext;
 
-        //const string dateFormat = "yyyy-MM-dd";
-        //public AccountController(UserManager<User> userManager, SignInManager<User> signInManager, MyBookContext db)
-        //{
-        //    _userManager = userManager;
-        //    _signInManager = signInManager;
-        //    _bookContext = db;
-        //}
-        //[HttpGet]
-        //public IActionResult Registration()
-        //{
-        //    return View();
-        //}
+        const string dateFormat = "yyyy-MM-dd";
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager, MyBookContext db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _bookContext = db;
+        }
+        [HttpGet]
+        public IActionResult Registration()
+        {
+            return View();
+        }
 
-        //[HttpPost]
-        //public async Task<IActionResult> Registration(RegistrationModel model)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        if (_userManager.Users.Any(userIdentity => userIdentity.Email == model.Email))
-        //        {
-        //            ModelState.AddModelError(string.Empty, "Пользователь с таким email уже существует");
-        //            return View(model);
-        //        }
-        //        if (model.BirthDate == null)
-        //        {
-        //            ModelState.AddModelError(string.Empty, "Введите корректную дату рождения");
-        //            return View(model);
-        //        }
-        //        User user = new User
-        //        {
-        //            Email = model.Email,
-        //            UserName = model.Email,
-        //            FirstName = model.FirstName,
-        //            LastName = model.LastName,
-        //            BirthDate = model.BirthDate?.ToShortDateString()
-        //        };
-        //        var result = await _userManager.CreateAsync(user, model.Password);
-        //        if (result.Succeeded)
-        //        {
-        //            await _userManager.AddClaimAsync(user, new Claim(ClaimTypes.Role, "Reader"));
-        //            _bookContext.SaveChanges();
-        //            await _signInManager.SignInAsync(user, false);
-        //            return RedirectToAction("Index", "Home");
-        //        }
-        //        else
-        //        {
-        //            foreach (var error in result.Errors)
-        //            {
-        //                ModelState.AddModelError(string.Empty, error.Description);
-        //            }
-        //        }
-        //    }
-        //    return View(model);
-        //}
+        [HttpPost]
+        public async Task<IActionResult> Registration(RegistrationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_userManager.Users.Any(userIdentity => userIdentity.Email == model.Email))
+                {
+                    ModelState.AddModelError(string.Empty, "Пользователь с таким email уже существует");
+                    return View(model);
+                }
+                if (model.BirthDate == null)
+                {
+                    ModelState.AddModelError(string.Empty, "Введите корректную дату рождения");
+                    return View(model);
+                }
+                User user = new User
+                {
+                    Email = model.Email,
+                    UserName = model.Email,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    BirthDate = model.BirthDate?.ToShortDateString()
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await _userManager.AddClaimAsync(user, new Claim(ClaimTypes.Role, "Reader"));
+                    _bookContext.SaveChanges();
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
 
-        //[HttpGet]
-        //public IActionResult Login(string returnUrl = null)
-        //{
-        //    return View(new SignInModel { ReturnUrl = returnUrl });
-        //}
+        [HttpGet]
+        public IActionResult Login(string returnUrl = null)
+        {
+            return View(new SignInModel { ReturnUrl = returnUrl });
+        }
 
-        //[HttpPost]
-        //public async Task<IActionResult> Login(SignInModel model)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        var result =
-        //            await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
-        //        if (result.Succeeded)
-        //        {
-        //            if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
-        //            {
-        //                return Redirect(model.ReturnUrl);
-        //            }
-        //            else
-        //            {
-        //                return RedirectToAction("Index", "Home");
-        //            }
-        //        }
-        //        else
-        //        {
-        //            ModelState.AddModelError("", "Неправильный логин и (или) пароль");
-        //        }
-        //    }
-        //    return View(model);
-        //}
+        [HttpPost]
+        public async Task<IActionResult> Login(SignInModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result =
+                    await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                    {
+                        return Redirect(model.ReturnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Неправильный логин и (или) пароль");
+                }
+            }
+            return View(model);
+        }
 
         [HttpPost]
         public async Task<IActionResult> Logout()
         {
-            //await _signInManager.SignOutAsync();
+            await _signInManager.SignOutAsync();
             return RedirectToAction("Index", "Home");
         }
     }
--- MyBook/Controllers/UserProfileController.cs
+++ MyBook/Controllers/UserProfileController.cs
@@ -8,102 +8,102 @@ using System.Text.RegularExpressions;
 
 namespace MyBook.Controllers
 {
-    //[Authorize(Policy = "ReadersOnly")]
+    [Authorize(Policy = "ReadersOnly")]
     public class UserProfileController : Controller
     {
-        //private readonly UserManager<User> _userManager;
-        //public UserProfileController(UserManager<User> userManager)
-        //{
-        //    _userManager = userManager;
-        //}
+        private readonly UserManager<User> _userManager;
+        public UserProfileController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
 
-        //[HttpGet]
-        //public async Task<IActionResult> EditProfile(string id)
-        //{
-        //    User user = await _userManager.FindByIdAsync(id);
-        //    if (user == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    EditProfileViewModel model = new EditProfileViewModel
-        //    {
-        //        Id = user.Id,
-        //        FirstName = user.FirstName,
-        //        LastName = user.LastName,
-        //        Email = user.Email,
-        //        BirthDate = DateTime.Parse(user.BirthDate)
-        //    };
-        //    return View(model);
-        //}
+        [HttpGet]
+        public async Task<IActionResult> EditProfile(string id)
+        {
+            User user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            EditProfileViewModel model = new EditProfileViewModel
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                BirthDate = DateTime.Parse(user.BirthDate)
+            };
+            return View(model);
+        }
 
-        //[HttpPost]
-        //public async Task<IActionResult> EditProfile(EditProfileViewModel model)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        User user = await _userManager.FindByIdAsync(model.Id);
-        //        if (user != null)
-        //        {
-        //            if (_userManager.Users.Any(userIdentity => userIdentity.Email == model.Email && userIdentity.UserName != user.UserName))
-        //            {
-        //                ModelState.AddModelError(string.Empty, "Пользователь с таким email уже существует");
-        //                return View(model);
-        //            }
-        //            if (model.BirthDate == null)
-        //            {
-        //                ModelState.AddModelError(string.Empty, "Введите корректную дату рождения");
-        //                return View(model);
-        //            }
-        //            user.Email = model.Email;
-        //            user.UserName = model.Email;
-        //            user.BirthDate = model.BirthDate?.ToShortDateString();
-        //            user.FirstName = model.FirstName;
-        //            user.LastName = model.LastName;
+        [HttpPost]
+        public async Task<IActionResult> EditProfile(EditProfileViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await _userManager.FindByIdAsync(model.Id);
+                if (user != null)
+                {
+                    if (_userManager.Users.Any(userIdentity => userIdentity.Email == model.Email && userIdentity.UserName != user.UserName))
+                    {
+                        ModelState.AddModelError(string.Empty, "Пользователь с таким email уже существует");
+                        return View(model);
+                    }
+                    if (model.BirthDate == null)
+                    {
+                        ModelState.AddModelError(string.Empty, "Введите корректную дату рождения");
+                        return View(model);
+                    }
+                    user.Email = model.Email;
+                    user.UserName = model.Email;
+                    user.BirthDate = model.BirthDate?.ToShortDateString();
+                    user.FirstName = model.FirstName;
+                    user.LastName = model.LastName;
 
-        //            var result = await _userManager.UpdateAsync(user);
-        //            if (result.Succeeded)
-        //            {
-        //                return RedirectToAction("EditProfile");
-        //            }
-        //            else
-        //            {
-        //                foreach (var error in result.Errors)
-        //                {
-        //                    ModelState.AddModelError(string.Empty, error.Description);
-        //                }
-        //            }
-        //        }
-        //    }
-        //    return View(model);
-        //}
+                    var result = await _userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("EditProfile");
+                    }
+                    else
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty, error.Description);
+                        }
+                    }
+                }
+            }
+            return View(model);
+        }
 
-        //[HttpPost]
-        //public async Task<IActionResult> EditPassword(EditPasswordViewModel model)
-        //{
-        //    //Почему то не отображается валидация
-        //    if (ModelState.IsValid)
-        //    {
-        //        User user = await _userManager.FindByIdAsync(model.Id);
-        //        if (user != null)
-        //        {
-        //            var result = await _userManager.ChangePasswordAsync(user, model.OldPassword, model.NewPassword);
-        //            if (result.Succeeded)
-        //            {
-        //                return RedirectToAction("EditProfile", new { model.Id });
-        //            }
-        //            else
-        //            {
-        //                foreach (var error in result.Errors)
-        //                {
-        //                    if (error.Description == "Incorrect password.")
-        //                        ModelState.AddModelError(string.Empty, "Введите верный старый пароль");
-        //                    else ModelState.AddModelError(string.Empty, error.Description);
-        //                }
-        //            }
-        //        }
-        //    }
-        //    return RedirectToAction("EditProfile", new { model.Id });
-        //}
+        [HttpPost]
+        public async Task<IActionResult> EditPassword(EditPasswordViewModel model)
+        {
+            //Почему то не отображается валидация
+            if (ModelState.IsValid)
+            {
+                User user = await _userManager.FindByIdAsync(model.Id);
+                if (user != null)
+                {
+                    var result = await _userManager.ChangePasswordAsync(user, model.OldPassword, model.NewPassword);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("EditProfile", new { model.Id });
+                    }
+                    else
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            if (error.Description == "Incorrect password.")
+                                ModelState.AddModelError(string.Empty, "Введите верный старый пароль");
+                            else ModelState.AddModelError(string.Empty, error.Description);
+                        }
+                    }
+                }
+            }
+            return RedirectToAction("EditProfile", new { model.Id });
+        }
         public IActionResult EditProfile()
         {
             return View();
