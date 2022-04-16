5992969b76beefadabecad249252a0d897409024
LeraRakhmankulova <lerakhmankulova@mail.ru>
2022-04-16 14:30:54 +0300

    clickable links

---
+++ MyBook/wwwroot/js/scroll.js
@@ -0,0 +1,16 @@
+﻿function anim(duration) {
+    var temp;
+    return function (sel) {
+        cancelAnimationFrame(temp);
+        var start = performance.now();
+        var from = window.pageYOffset || document.documentElement.scrollTop,
+            to = document.querySelector(sel).getBoundingClientRect().top;
+        requestAnimationFrame(function step(timestamp) {
+            var progress = (timestamp - start) / duration;
+            1 <= progress && (progress = 1);
+            window.scrollTo(0, from + to * progress | 0);
+            1 > progress && (temp = requestAnimationFrame(step))
+        })
+    }
+};
+var scrollMenu = anim(300)
