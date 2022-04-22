ee046c97587b6e3af0307dcc7f7eec003e8e6719
Elizav555 <lizagarkina5@gmail.com>
2022-04-22 10:33:07 +0300

    refactored paging

---
+++ MyBook/Models/Admin/AdminViewModel.cs
@@ -0,0 +1,13 @@
+﻿using MyBook.Entities;
+
+namespace MyBook.Models.Admin
+{
+    public class AdminViewModel
+    {
+        public List<User> Users { get; set; }
+        public List<MyBook.Entities.Type> SubscrTypes { get; set; } = new();
+        public List<BookCenter> Centers { get; set; }
+        public List<Book> Books { get; set; }
+        public List<Author> Authors { get; set; } = new();
+    }
+}
