d6da88b3819cef498ef795dc20c3bc032741ba4f
newtmas1956 <82896663+newtmas1956@users.noreply.github.com>
2022-04-24 14:20:17 +0300

    add interfaces and implementations for services

---
+++ MyBook/Services/IGenresFilterGetter.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using MyBook.Infrastructure.Repositories;
+
+namespace MyBook.Infrastructure.Services;
+
+public interface IGenresFilterGetter
+{
+    List<SelectListItem> GetItems(EFGenreRepository repository);
+}
