05437c3e5ab49163f5621cf01fc3704b57dfaac4
DamirGuzaerov <86714479+DamirGuzaerov@users.noreply.github.com>
2022-04-29 22:35:18 +0300

    authors edit pagination added+ authors edit search added

--- MyBook/wwwroot/js/searchBooks.js
+++ MyBook/wwwroot/js/searchBooks.js
@@ -1,21 +1,20 @@
-﻿let pageNumber = 0;
+﻿let pageNumber = 1;
 function showMoreBooks(){
-    alert("ABOBA")
     $.ajax(
         {
             type: "GET",
             url:`/Search/SearchBooks?page=${++pageNumber}`,
             success: function (data) {
                 var $response=$(data);
-                console.log(data)
                 var resultDiv = $response.find("#list").html();
-                $("#list").append(resultDiv);
+                $("#list").html(resultDiv);
             },
             error: function (data){
                 alert("Чота не так пошло");
             }
         }
     )
+    return false;
 }
 
 
