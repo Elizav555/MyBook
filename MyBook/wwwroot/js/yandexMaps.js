011ab4705457e766c26313020d6ace38cf73762c
DamirGuzaerov <86714479+DamirGuzaerov@users.noreply.github.com>
2022-05-11 21:39:32 +0300

    yandex map added

---
+++ MyBook/wwwroot/js/yandexMaps.js
@@ -0,0 +1,15 @@
+﻿ymaps.ready(init);
+
+function init(){
+    var myMap = new ymaps.Map("map", {
+        center: [55.76, 37.64],
+        zoom: 13
+    });
+
+    myMap.geoObjects.add(new ymaps.Placemark([55.753048, 37.611111], {
+        balloonContent: '<strong>Тут наш центр</strong>'
+    }, {
+        preset: 'islands#circleDotIcon',
+        iconColor: 'yellow'
+    }))
+}
