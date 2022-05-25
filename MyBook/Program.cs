7af882ce371f6af3ddd2244fa40858d9a27fb266
DamirGuzaerov <86714479+DamirGuzaerov@users.noreply.github.com>
2022-05-25 12:48:21 +0300

    cors configured

--- MyBook/Program.cs
+++ MyBook/Program.cs
@@ -3,9 +3,11 @@ using MyBook.Infrastructure.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors();
 builder.ConfigureServices(builder.Configuration);
 
 var app = builder.Build();
+app.UseCors(builder => builder.WithOrigins("https://my-book-app-kpfu-proj.herokuapp.com"));
 app.UseCookiePolicy(new CookiePolicyOptions()
 {
     MinimumSameSitePolicy = SameSiteMode.Lax
