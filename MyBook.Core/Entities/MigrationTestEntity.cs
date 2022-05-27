fc13fb482e0bebc246c709ec1ed62b305392c58a
DamirGuzaerov <86714479+DamirGuzaerov@users.noreply.github.com>
2022-05-27 22:25:25 +0300

    MigrationTest commit

---
+++ MyBook.Core/Entities/MigrationTestEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace MyBook.Entities
+{
+    public class MigrationTestEntity
+    {
+        public int MigrationTestEntityId { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
